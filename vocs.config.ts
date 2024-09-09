9362f52c79591e797f1e32448745dca95845f301
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-09-09 09:49:29 +0530

    test arch image

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vocs'
+import { defineConfig } from "vocs";
 
 export default defineConfig({
-  title: 'Docs',
+  title: "Docs",
   sidebar: [
     {
-      text: 'Getting Started',
-      link: '/getting-started',
+      text: "Getting Started",
+      link: "/getting-started",
     },
     {
-      text: 'Example',
-      link: '/example',
+      text: "Architecture",
+      link: "/architecture",
+    },
+    {
+      text: "Tech Stack",
+      link: "/tech-stack",
     },
   ],
-})
+});
